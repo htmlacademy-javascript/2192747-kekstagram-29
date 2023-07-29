335b51a13c3f35a7455974d890d1d8a5543d4cbf
Tamara <chervonaoka111@yandex.ru>
2023-07-29 14:44:10 +0300

    Готовая функция

--- js/data.js
+++ js/data.js
@@ -35,46 +35,49 @@ const NAME = [
   'Мила',
 ];
 
-const ID = [];
-
-while (ID.length !== DESCRIPTION_ARRAY_LENGTH) {
-  const generatedItems = [];
-  generatedItems.push(getRandomInteger(1, DESCRIPTION_ARRAY_LENGTH));
-  const itemCheck = (item) => {
-    if (ID.indexOf(item) === -1) {
-      ID.push(item);
-      return true;
-    }
-    return false;
-  };
-  generatedItems.filter((item) => itemCheck(item));
-}
+const generateIDs = (arrayLength) => {
+  const Ids = [];
+  while (Ids.length !== arrayLength) {
+    const generatedItems = [];
+    generatedItems.push(getRandomInteger(1, arrayLength));
+    const itemCheck = (item) => {
+      if (Ids.indexOf(item) === -1) {
+        Ids.push(item);
+        return true;
+      }
+      return false;
+    };
+    generatedItems.filter((item) => itemCheck(item));
+  }
+  return Ids;
+};
 
 const createMessage = () => Array.from({
   length: getRandomInteger(1, 3)
-}, () => getRandomArrayElement(MESSAGE)).join('');
+}, () => getRandomArrayElement(MESSAGE), ).join('');
 
-const makeComment = (commentIndex) => ({
-  id: commentIndex,
-  avatar: `img/${ commentIndex }.svg`,
+const makeComment = (commentId) => ({
+  id: commentId,
+  avatar: `img/${ commentId }.svg`,
   message: createMessage(),
   name: getRandomArrayElement(NAME, NAME.length - 1),
 });
 
-const createPhotoDescription = (index) => ({
-  id: ID[index],
-  url: `photos/${ ID[index] }.jpg`,
+const createPhotoDescription = (id) => ({
+  id: id,
+  url: `photos/${ id }.jpg`,
   description: getRandomArrayElement(PHOTO_DESCRIPTION, PHOTO_DESCRIPTION.length - 1),
   likes: getRandomInteger(LIKES_MIN_COUNT, LIKES_MAX_COUNT),
-  comments: Array.from({
-    length: COMMENTS_MAX_COUNT
-  }, ((_, commentIndex) =>
-    makeComment(commentIndex + 1)))
+  comment: (function () {
+    const uniqueCommentId = generateIDs(COMMENTS_MAX_COUNT);
+    return uniqueCommentId.map((commentId) => makeComment(commentId));
+  })()
 });
 
-const generateDescriptionArray = () => Array.from({
-  length: DESCRIPTION_ARRAY_LENGTH
-}, (_, index) => createPhotoDescription(index + 1));
+const generateDescriptionArray = () => {
+  const uniqueId = generateIDs(DESCRIPTION_ARRAY_LENGTH);
+  return uniqueId.map((id) => createPhotoDescription(id));
+};
 
 export {
   generateDescriptionArray
